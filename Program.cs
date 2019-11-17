a06e37369d3afd7c5cd73f6bf7a6c26b8d371b1b
R0M41Q <46008056+R0M41Q@users.noreply.github.com>
2019-11-17 03:27:44 +0200

    first update

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+
+namespace TelegramBotHooyLeeKey
+{
+    class Program
+    {
+        public static ITelegramBotClient botclient;
+        static void Main(string[] args)
+        {
+            botclient = new TelegramBotClient("946377878:AAEGAoWALvdnLDtQobbbh80l-JlemR-F8Rs");
+
+            var me = botclient.GetMeAsync().Result;
+            Console.WriteLine($"bot id: { me.Id } bot name: { me.FirstName } ");
+
+            botclient.OnMessage += message;
+            botclient.StartReceiving();
+            Console.ReadLine();
+            
+        }
+            private static async void message(object sender, MessageEventArgs e)
+        {
+            var text = e?.Message?.Text;
+            if (text == null) return;
+            if (text.Contains("Тьома підар"))
+            {
+                await botclient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        $"Мельник більший підар!",
+                        replyToMessageId: e.Message.MessageId
+                    ).ConfigureAwait(false);
+            }
+            else
+            if (text.Contains("Тьома"))
+            {
+                await botclient.SendTextMessageAsync(
+                        chatId: e.Message.Chat.Id,
+                        $"Ти хотів сказати Тьома підар?",
+                        replyToMessageId: e.Message.MessageId
+                    ).ConfigureAwait(false);
+            }
+
+        }
+        
+    }
+}
