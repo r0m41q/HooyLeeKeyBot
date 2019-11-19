4edcd4ae7398eea913bfd547c2eaee74a964cc70
R0M41Q <46008056+R0M41Q@users.noreply.github.com>
2019-11-19 21:29:42 +0200

    added docker, heroku, circleci

--- Program.cs
+++ Program.cs
@@ -8,24 +8,34 @@ namespace TelegramBotHooyLeeKey
     class Program
     {
         public static ITelegramBotClient botclient;
+        
+
         static void Main(string[] args)
         {
             botclient = new TelegramBotClient("946377878:AAEGAoWALvdnLDtQobbbh80l-JlemR-F8Rs");
-
+            
             var me = botclient.GetMeAsync().Result;
             Console.WriteLine($"bot id: { me.Id } bot name: { me.FirstName } ");
 
             botclient.OnMessage += message;
+            
             botclient.StartReceiving();
-            Console.ReadLine();
+            
+            
+                Console.ReadLine();
+               
             
         }
+
+        
+
             private static async void message(object sender, MessageEventArgs e)
-        {
+            {
             var text = e?.Message?.Text;
             if (text == null) return;
             if (text.Contains("Тьома підар"))
             {
+                
                 await botclient.SendTextMessageAsync(
                         chatId: e.Message.Chat.Id,
                         $"Мельник більший підар!",
@@ -37,12 +47,13 @@ namespace TelegramBotHooyLeeKey
             {
                 await botclient.SendTextMessageAsync(
                         chatId: e.Message.Chat.Id,
-                        $"Ти хотів сказати Тьома підар?",
-                        replyToMessageId: e.Message.MessageId
+                        $"Ти хотів сказати Тьома підар?"
+                        , replyToMessageId: e.Message.MessageId
                     ).ConfigureAwait(false);
             }
+            Console.WriteLine("Message recieved");
 
-        }
+            }
         
     }
 }
